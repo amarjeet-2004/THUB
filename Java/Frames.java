5aa819ff111090812bc71593ab85b17a50dd10cd
amarjeet-2004 <165481488+amarjeet-2004@users.noreply.github.com>
2024-10-27 12:01:09 +0530

    Update Frames.java

--- Java/Frames.java
+++ Java/Frames.java
@@ -1,1 +1,13 @@
-hi
+import java.util.*;
+public class frame
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int x=sc.nextInt();
+        int total_cost=(n+m+n+m)*x;
+        System.out.println(total_cost);
+    }
+}
